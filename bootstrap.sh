6e646fca5850967370bd00bb04db5eddc75d86af
Wim Mostmans <wim@sitebase.be>
2013-02-08 11:20:18 +0100

    add echo that tells where the dotfiles are synced

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@ cd "$(dirname "${BASH_SOURCE}")"
 git pull
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+	echo "Synced dotfiles with: "`pwd`
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
