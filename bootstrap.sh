545df4b1a129d612bea8e4a00fc895f1389c4b84
Wim Mostmans <wim@sitebase.be>
2013-12-27 22:43:29 +0100

    add zshrc file and ignore copy of oh-my-zsh folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+	rsync --exclude "oh-my-zsh/" --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
 	echo "Synced dotfiles with: "`pwd`
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
